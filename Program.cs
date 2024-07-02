4979e4ee28b245f2a0cc60bd030de7ee178022c1
GeorgeSobhy <georgeaziz001@gmail.com>
2024-07-02 21:24:09 +0300

    Create & delete folder and Copy file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string workingPath = @"D:\career\programming\.net\codes\working with files\working path";
+
+
+string copiedDir = Path.Combine(workingPath, "copied");
+
+if (!Directory.Exists(copiedDir))
+    Console.WriteLine("I'am gonna create new directory");
+
+Directory.CreateDirectory(copiedDir);
+
+
+if (!File.Exists(Path.Combine(workingPath, "text.txt")))
+{
+    Console.WriteLine($"there is no file in {workingPath}");
+    return;
+}
+File.Copy(Path.Combine(workingPath,"text.txt"), Path.Combine(copiedDir, "text.txt"),true);
+
+string redundantFolder = Path.Combine(workingPath, "redundant directory");
+Directory.Delete(redundantFolder);
